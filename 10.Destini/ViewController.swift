2f3daa40054af9ba0258d8676a2026e2bcda2c61
Heny <hyoh0606@gmail.com>
2021-04-27 17:18:55 +0900

    Initial Commit

---
+++ 10.Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10.Destini
+//
+//  Created by 오현호 on 2021/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
